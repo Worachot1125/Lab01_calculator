7e99c0cdc45fb715670f3a5652fad5c35ebd0c94
วรโชติ ทองเลิศ <119285571+Worachot1125@users.noreply.github.com>
2022-12-26 19:00:42 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace Lab01
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.tb_Input = new System.Windows.Forms.TextBox();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.process = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button1.Font = new System.Drawing.Font("Segoe UI Symbol", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button1.Location = new System.Drawing.Point(146, 205);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(46, 46);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "7";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button2.Font = new System.Drawing.Font("Segoe UI Symbol", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button2.Location = new System.Drawing.Point(198, 205);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(46, 46);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "8";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button3.Font = new System.Drawing.Font("Segoe UI Symbol", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button3.Location = new System.Drawing.Point(250, 205);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(46, 46);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "9";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button4.Font = new System.Drawing.Font("Segoe UI Symbol", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button4.Location = new System.Drawing.Point(146, 257);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(46, 46);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button5.Font = new System.Drawing.Font("Segoe UI Symbol", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button5.Location = new System.Drawing.Point(198, 257);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(46, 46);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button6.Font = new System.Drawing.Font("Segoe UI Symbol", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button6.Location = new System.Drawing.Point(250, 257);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(46, 46);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button7.Font = new System.Drawing.Font("Segoe UI Symbol", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button7.Location = new System.Drawing.Point(146, 309);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(46, 46);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "1";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button8.Font = new System.Drawing.Font("Segoe UI Symbol", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button8.Location = new System.Drawing.Point(198, 309);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(46, 46);
+            this.button8.TabIndex = 13;
+            this.button8.Text = "2";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button9.Font = new System.Drawing.Font("Segoe UI Symbol", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button9.Location = new System.Drawing.Point(250, 309);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(46, 46);
+            this.button9.TabIndex = 14;
+            this.button9.Text = "3";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button10.Font = new System.Drawing.Font("Segoe UI Symbol", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button10.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button10.Location = new System.Drawing.Point(146, 361);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(46, 46);
+            this.button10.TabIndex = 15;
+            this.button10.Text = "0";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button11.Font = new System.Drawing.Font("Segoe UI Symbol", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button11.ForeColor = System.Drawing.Color.Red;
+            this.button11.Location = new System.Drawing.Point(243, 361);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(72, 46);
+            this.button11.TabIndex = 16;
+            this.button11.Text = "=";
+            this.button11.UseVisualStyleBackColor = false;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.Color.Olive;
+            this.button12.Font = new System.Drawing.Font("Segoe UI Symbol", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button12.ForeColor = System.Drawing.Color.Lime;
+            this.button12.Location = new System.Drawing.Point(315, 205);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(46, 46);
+            this.button12.TabIndex = 17;
+            this.button12.Text = "+";
+            this.button12.UseVisualStyleBackColor = false;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.BackColor = System.Drawing.Color.Olive;
+            this.button13.Font = new System.Drawing.Font("Segoe UI Symbol", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button13.ForeColor = System.Drawing.Color.Lime;
+            this.button13.Location = new System.Drawing.Point(315, 257);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(46, 46);
+            this.button13.TabIndex = 18;
+            this.button13.Text = "-";
+            this.button13.UseVisualStyleBackColor = false;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.BackColor = System.Drawing.Color.Olive;
+            this.button14.Font = new System.Drawing.Font("Segoe UI Symbol", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button14.ForeColor = System.Drawing.Color.Lime;
+            this.button14.Location = new System.Drawing.Point(315, 309);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(46, 46);
+            this.button14.TabIndex = 19;
+            this.button14.Text = "*";
+            this.button14.UseVisualStyleBackColor = false;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // button15
+            // 
+            this.button15.BackColor = System.Drawing.Color.Olive;
+            this.button15.Font = new System.Drawing.Font("Segoe UI Symbol", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button15.ForeColor = System.Drawing.Color.Lime;
+            this.button15.Location = new System.Drawing.Point(315, 361);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(46, 46);
+            this.button15.TabIndex = 20;
+            this.button15.Text = "/";
+            this.button15.UseVisualStyleBackColor = false;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // tb_Input
+            // 
+            this.tb_Input.Font = new System.Drawing.Font("Segoe UI Symbol", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tb_Input.Location = new System.Drawing.Point(146, 82);
+            this.tb_Input.Name = "tb_Input";
+            this.tb_Input.Size = new System.Drawing.Size(215, 43);
+            this.tb_Input.TabIndex = 21;
+            // 
+            // button16
+            // 
+            this.button16.BackColor = System.Drawing.Color.Red;
+            this.button16.Font = new System.Drawing.Font("Segoe UI Symbol", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button16.ForeColor = System.Drawing.Color.Black;
+            this.button16.Location = new System.Drawing.Point(294, 133);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(67, 66);
+            this.button16.TabIndex = 22;
+            this.button16.Text = "C";
+            this.button16.UseVisualStyleBackColor = false;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // button17
+            // 
+            this.button17.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button17.Font = new System.Drawing.Font("Segoe UI Symbol", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button17.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button17.Location = new System.Drawing.Point(196, 361);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(46, 46);
+            this.button17.TabIndex = 23;
+            this.button17.Text = ".";
+            this.button17.UseVisualStyleBackColor = false;
+            this.button17.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // process
+            // 
+            this.process.Location = new System.Drawing.Point(196, 49);
+            this.process.Name = "process";
+            this.process.Size = new System.Drawing.Size(165, 27);
+            this.process.TabIndex = 24;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 487);
+            this.Controls.Add(this.process);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.tb_Input);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private TextBox tb_Input;
+        private Button button16;
+        private Button button17;
+        private TextBox process;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,170 @@
+﻿using System.Drawing.Text;
+using System.Security.Cryptography;
+
+namespace Lab01
+{
+    public partial class Form1 : Form
+    {
+        //class variable
+        double input1 = 0;
+        String operation = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // ปุ่ม 0
+        private void button10_Click(object sender, EventArgs e)
+        {
+            this.tb_Input.Text = this.tb_Input.Text + "0";
+            this.process.Text = this.process.Text + "0";
+
+        }
+
+        // ปุ่ม 1
+        private void button7_Click(object sender, EventArgs e)
+        {
+            this.tb_Input.Text = this.tb_Input.Text + "1";
+            this.process.Text = this.process.Text + "1";
+        }
+
+        // ปุ่ม 2
+        private void button8_Click(object sender, EventArgs e)
+        {
+            this.tb_Input.Text = this.tb_Input.Text + "2";
+            this.process.Text = this.process.Text + "2";
+        }
+
+        // ปุ่ม 3
+        private void button9_Click(object sender, EventArgs e)
+        {
+            this.tb_Input.Text = this.tb_Input.Text + "3";
+            this.process.Text = this.process.Text + "3";
+        }
+
+        // ปุ่ม 4
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.tb_Input.Text = this.tb_Input.Text + "4";
+            this.process.Text = this.process.Text + "4";
+        }
+
+        // ปุ่ม 5
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.tb_Input.Text = this.tb_Input.Text + "5";
+            this.process.Text = this.process.Text + "5";
+        }
+
+        // ปุ่ม 6
+        private void button6_Click(object sender, EventArgs e)
+        {
+            this.tb_Input.Text = this.tb_Input.Text + "6";
+            this.process.Text = this.process.Text + "6";
+        }
+
+        // ปุ่ม 7
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.tb_Input.Text = this.tb_Input.Text + "7";
+            this.process.Text = this.process.Text + "7";
+        }
+
+        // ปุ่ม 8
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.tb_Input.Text = this.tb_Input.Text + "8";
+            this.process.Text = this.process.Text + "8";
+        }
+
+        // ปุ่ม 9
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.tb_Input.Text = this.tb_Input.Text + "9";
+            this.process.Text = this.process.Text + "9";
+        }
+
+        // ปุ่ม +
+        private void button12_Click(object sender, EventArgs e)
+        {
+            //assign value to class variable
+            this.input1 = Double.Parse(this.tb_Input.Text);
+            //clear input text
+            this.tb_Input.Text = "";
+            this.operation = "+";
+            this.process.Text = this.process.Text + "+";
+        }
+
+        // ปุ่ม -
+        private void button13_Click(object sender, EventArgs e)
+        {
+            //assign value to class variable
+            this.input1 = Double.Parse(this.tb_Input.Text);
+            //clear input text
+            this.tb_Input.Text = "";
+            this.operation = "-";
+            this.process.Text = this.process.Text + "-";
+        }
+
+        // ปุ่ม *
+        private void button14_Click(object sender, EventArgs e)
+        {
+            //assign value to class variable
+            this.input1 = Double.Parse(this.tb_Input.Text);
+            //clear input text
+            this.tb_Input.Text = "";
+            this.operation = "*";
+            this.process.Text = this.process.Text + "*";
+        }
+
+        // ปุ่ม /
+        private void button15_Click(object sender, EventArgs e)
+        {
+            //assign value to class variable
+            this.input1 = Double.Parse(this.tb_Input.Text);
+            //clear input text
+            this.tb_Input.Text = "";
+            this.operation = "/";
+            this.process.Text = this.process.Text + "/";
+
+        }
+
+        // ปู่ม .
+        private void button17_Click(object sender, EventArgs e)
+        {
+            this.tb_Input.Text = this.tb_Input.Text + ".";
+            this.process.Text = this.process.Text + ".";
+        }
+
+        // ปุ่ม =
+        private void button11_Click(object sender, EventArgs e)
+        {
+            double input2 = Double.Parse(this.tb_Input.Text);
+            double result = 0;
+            if (this.operation == "+")
+            {
+                result = input1 + input2;
+            }
+            else if (this.operation == "-")
+            {
+                result = input1 - input2;
+            }
+            else if (this.operation == "*")
+            {
+                result = input1 * input2;
+            }
+            else if (this.operation == "/")
+            {
+                result = input1 / input2;
+            }
+            this.tb_Input.Text = result.ToString("#,##0");
+            this.process.Text = this.process.Text + "=";
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            this.tb_Input.Text = "";
+            this.process.Text = "";
+        }
+    }
+}
